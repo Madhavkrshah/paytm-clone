ebc063a417a3dfcecba554e31cd577bce7f8083c
Madhavkrshah <madhavkrshah@gmail.com>
2026-03-16 17:57:03 +0530

    Initial backend code

---
+++ backend/db.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const dbUrl =
+  "mongodb+srv://paytm_user:J7qfqm7U6UC8wfrp@cluster0.25cqayt.mongodb.net/";
+
+async function connectDB() {
+  try {
+    await mongoose.connect(dbUrl);
+    console.log(`MongoDB connected successfully`);
+  } catch (err) {
+    console.log(`MongoDB connectioin error ${err}`);
+  }
+}
+connectDB();
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const mainRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const JWT_SECRET = require('./config')
+const app = express();
+const port = 3000;
 
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/", mainRouter);
+app.use("/api/v1/user", userRouter);
 
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
